e7c376b981b4228092e3c562287546aeeb504b02
abdurahmonanvarov <dousolo9@gmail.com>
2025-04-27 08:20:56 +0500

    user added fix

--- src/component/FillJob.tsx
+++ src/component/FillJob.tsx
@@ -6,8 +6,16 @@ function FillJob() {
   const [loading, setLoading] = useState(false);
 
   const handleRequest = () => {
+    const token = localStorage.getItem("token"); // tokenni localStorage'dan olamiz
+
+    if (!token) {
+      toast.error("Iltimos, ro'yxatdan o'ting (register) ❌");
+      return; // Token yo'q bo'lsa request yuborilmaydi
+    }
+
     setLoading(true);
 
+    // Token mavjud bo'lsa, request yuboriladi
     setTimeout(() => {
       setLoading(false);
       toast.success("Muvaffaqiyatli qo'shildi ✅");
